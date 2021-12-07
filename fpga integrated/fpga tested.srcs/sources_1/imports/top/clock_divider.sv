6e3a0103ccdb4fb846974b822119e47f20b37728
nameer-iqbal-anasri <n.i.ansari200@gmail.com>
2021-12-08 02:02:24 +0500

    fpga files added

---
+++ fpga integrated/fpga tested.srcs/sources_1/imports/top/clock_divider.sv
@@ -0,0 +1,17 @@
+module Clock_divider(input logic clock_in,output logic clock_out
+    );
+reg[27:0] counter=28'd0;
+parameter DIVISOR = 28'd2;
+// The frequency of the output clk_out
+//  = The frequency of the input clk_in divided by DIVISOR
+// For example: Fclk_in = 50Mhz, if you want to get 1Hz signal to blink LEDs
+// You will modify the DIVISOR parameter value to 28'd50.000.000
+// Then the frequency of the output clk_out = 50Mhz/50.000.000 = 1Hz
+always @(posedge clock_in)
+begin
+ counter <= counter + 28'd1;
+ if(counter>=(DIVISOR-1))
+  counter <= 28'd0;
+ clock_out <= (counter<DIVISOR/2)?1'b1:1'b0;
+end
+endmodule
